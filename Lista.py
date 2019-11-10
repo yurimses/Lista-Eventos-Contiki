5d4b632fcc571befd4bbd51a9b53501e97c8c7c3
Yuri <yuri_mses@hotmail.com>
2019-11-09 22:20:22 -0300

    Lista Comentada

--- Lista.py
+++ Lista.py
@@ -9,8 +9,14 @@ print('Intervalo de tempo: {} min'.format(tempo))
 for i in range(tempo):
     array = []
     array = random.sample(range(0,nos), random.randint(0, nos))
+    #random.sample(população(intervalo), amostra(intervalo))  
     if 0 in array:
         array.remove(0)
     if 1 in array:
         array.remove(1)
     print('minuto {} / evento nos nós: {} '.format(i, array))
+
+'''
+Falta jogar dados do laço para um arquivo (é necessário criar uma lista para cada iteração?)
+Tem algum minuto que não ocorre evento? (como interagir listas vazias com C?)
+'''
