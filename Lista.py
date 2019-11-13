74bcafa6edbe2fab335f8b9cd678ecd3280eca06
yurimses <yuri_mses@hotmail.com>
2019-11-13 14:49:33 -0300

    Adicionando comentários

--- Lista.py
+++ Lista.py
@@ -4,13 +4,14 @@ import random
 
 nos = int(input('Informe número de nós servidores: '))
 print('Número de nós na simulação: {}'.format(nos))
+#nos: determinar o número de nós servidores que estão sendo executados no Cooja 
 tempo = int(input('Informe intervalo de tempo em minutos: '))
 print('Intervalo de tempo: {} min'.format(tempo))
+#tempo: determinar intervalo de tempo de ocorrência de eventos na simulação
 arq = open('Eventos.txt','w')
 for i in range(tempo):
     array = []
     array = random.sample(range(0,nos), random.randint(0, nos))
-    #random.sample(população(intervalo), amostra(intervalo))  
     if 0 in array:
         array.remove(0)
     if 1 in array:
@@ -18,6 +19,10 @@ for i in range(tempo):
     print('minuto {} / evento nos nós: {} '.format(i, array))
     arq.write('{}\n'.format(array))
 arq.close()
+
 '''
-Tem algum minuto que não ocorre evento? (como interagir listas vazias com C?)
+array: utilizando estratégia de população e amostra
+1º argumento: intervalo da população e 2º argumento: amostra, responsável por determinar o tamanho de array (número de índices)
+random.sample(população(intervalo), amostra(intervalo))
+linhas 15 a 18: responsáveis por eliminar os numeros 0 e 1 (border router) dos vetores 
 '''
