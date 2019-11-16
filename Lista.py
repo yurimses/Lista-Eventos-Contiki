e727c67a38320bd0444008a8be375795e6aec83b
yurimses <yuri_mses@hotmail.com>
2019-11-16 01:38:59 -0300

    Correção

--- Lista.py
+++ Lista.py
@@ -1,28 +1,18 @@
 # -*- coding: utf-8 -*-
 import random
 
-
 nos = int(input('Informe número de nós servidores: '))
 print('Número de nós na simulação: {}'.format(nos))
-#nos: determinar o número de nós servidores que estão sendo executados no Cooja 
 tempo = int(input('Informe intervalo de tempo em minutos: '))
 print('Intervalo de tempo: {} min'.format(tempo))
-#tempo: determinar intervalo de tempo de ocorrência de eventos na simulação
 arq = open('Eventos.txt','w')
 for i in range(tempo):
-    array = []
-    array = random.sample(range(0,nos), random.randint(0, nos))
-    if 0 in array:
-        array.remove(0)
-    if 1 in array:
-        array.remove(1)
-    print('minuto {} / evento nos nós: {} '.format(i, array))
-    arq.write('{}\n'.format(array))
+    vetor = []
+    vetor = random.sample(range(nos+1), random.randint(0, nos+1))  
+    if 0 in vetor:
+        vetor.remove(0)
+    if 1 in vetor:
+        vetor.remove(1)
+    print('minuto {} / evento nos nós: {} / {} no total'.format(i+1, vetor, len(vetor)))
+    arq.write('{}\n'.format(vetor))
 arq.close()
-
-'''
-array: utilizando estratégia de população e amostra
-1º argumento: intervalo da população e 2º argumento: amostra, responsável por determinar o tamanho de array (número de índices)
-random.sample(população(intervalo), amostra(intervalo))
-linhas 15 a 18: responsáveis por eliminar os numeros 0 e 1 (border router) dos vetores 
-'''
