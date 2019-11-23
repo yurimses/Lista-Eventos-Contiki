7cc84f933644efb76b0e465c5672d0e3c32a8d51
Marlon W. Santos <marlon.santos.santos@icen.ufpa.br>
2019-11-22 22:48:11 -0300

    Adicionados variável e matriz de eventos

--- Lista.py
+++ Lista.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 import random
-import re
 
 nos = int(input('Informe número de nós servidores: '))
 print('Número de nós na simulação: {}'.format(nos))
 tempo = int(input('Informe intervalo de tempo em minutos: '))
 print('Intervalo de tempo: {} min'.format(tempo))
 arq = open('Eventos.h','w')
+arq.write('int numero_Motes='+str(nos)+'\n')
+arq.write('int id_nodes_ev['+str(tempo)+']['+str(nos)+']={')
 for i in range(tempo):
     array = []
     array = random.sample(range(0,nos), random.randint(0, nos))
@@ -17,8 +18,6 @@ for i in range(tempo):
         array.remove(1)
     print('minuto {} / evento nos nós: {} '.format(i, array))
     array=str(array).replace('[','{').replace(']','}')
-    arq.write('{}\n'.format(array))
+    arq.write('{},\n'.format(array))
+arq.write('}\n')
 arq.close()
-'''
-Tem algum minuto que não ocorre evento? (como interagir listas vazias com C?)
-'''
