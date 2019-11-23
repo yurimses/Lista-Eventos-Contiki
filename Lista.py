34fa0ffc1a74786cdd2bc95aa3a9ab97dbe89ead
Marlon W. Santos <marlon.santos.santos@icen.ufpa.br>
2019-11-22 23:00:19 -0300

    Print dos vetores

--- Lista.py
+++ Lista.py
@@ -6,7 +6,7 @@ print('Número de nós na simulação: {}'.format(nos))
 tempo = int(input('Informe intervalo de tempo em minutos: '))
 print('Intervalo de tempo: {} min'.format(tempo))
 arq = open('Eventos.h','w')
-arq.write('int numero_Motes='+str(nos)+'\n')
+arq.write('int numero_Motes='+str(nos)+';\n')
 arq.write('int id_nodes_ev['+str(tempo)+']['+str(nos)+']={')
 for i in range(tempo):
     array = []
@@ -16,8 +16,8 @@ for i in range(tempo):
         array.remove(0)
     if 1 in array:
         array.remove(1)
-    print('minuto {} / evento nos nós: {} '.format(i, array))
     array=str(array).replace('[','{').replace(']','}')
+    print('minuto {} / evento nos nós: {} '.format(i, array))
     arq.write('{},\n'.format(array))
-arq.write('}\n')
+arq.write('};\n')
 arq.close()
