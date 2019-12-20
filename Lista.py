2ef14235fa94af3429e021369b49d0e74d5dbbe2
Marlon W. Santos <marlon.santos.santos@icen.ufpa.br>
2019-12-20 12:33:00 -0300

    Licença

--- Lista.py
+++ Lista.py
@@ -2,6 +2,8 @@
        Lista.py is a generator of events
        Copyright (c) 2019 Yuri M. Ses <your-email@icen.ufpa.br>
        Copyright (c) 2019 Marlon W. Santos <marlon.santos.santos@icen.ufpa.br>
+
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
